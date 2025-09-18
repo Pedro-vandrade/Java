547d4cc7230ac62fcd7fada0279d7e0d27b38f47
Pedro Vargas <96157565+Pedro-vandrade@users.noreply.github.com>
2025-09-18 01:37:54 -0300

    Update Exerc09.java

--- Exerc09.java
+++ Exerc09.java
@@ -1,18 +1,29 @@
+package ifelse;
+
+import java.util.Scanner;
+
+
 public class Exerc09 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        double imposto;
+        System.out.println("Digite o Salario: ");
+        double salario = sc.nextDouble();
+// 2500
+        if (salario <= 2000.00 && salario <= 2999.99) {
+            System.out.printf("Sem taxa de imposto.");
+        } else if (salario >= 3000.00 && salario <= 4499.99) {
+            imposto = ((salario - 2000) * 8) / 100;
+            System.out.printf("Imposto a ser pago R$ %.2f%n: ", imposto);
+        } else if (salario >= 4500.00) {
+            imposto = ((salario - 2000) * 18) / 100;
+            System.out.printf("Imposto a ser pago R$ %.2f%n: ", imposto);
+            {
+
+            }
+        }
     }
 }
 
-/* Em um país imaginário denominado Lisarb, todos os habitantes ficam felizes em pagar seus impostos, pois sabem
-que nele não existem políticos corruptos e os recursos arrecadados são utilizados em benefício da população, sem
-qualquer desvio. A moeda deste país é o Rombus, cujo símbolo é o R$.
-Leia um valor com duas casas decimais, equivalente ao salário de uma pessoa de Lisarb. Em seguida, calcule e
-mostre o valor que esta pessoa deve pagar de Imposto de Renda, segundo a tabela abaixo.
 
-Lembre que, se o salário for R$ 3002.00, a taxa que incide é de 8% apenas sobre R$ 1000.00, pois a faixa de
-salário que fica de R$ 0.00 até R$ 2000.00 é isenta de Imposto de Renda. No exemplo fornecido (abaixo), a taxa é
-de 8% sobre R$ 1000.00 + 18% sobre R$ 2.00, o que resulta em R$ 80.36 no total. O valor deve ser impresso com
-duas casas decimais.
-
-*/
