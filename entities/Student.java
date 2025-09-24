9a0b50596eb4ed8e96bb72b3c7dde6dd950760ee
Pedro Vargas <96157565+Pedro-vandrade@users.noreply.github.com>
2025-09-24 11:36:30 -0300

    Add files via upload

---
+++ entities/Student.java
@@ -0,0 +1,19 @@
+package entities;
+
+public class Student {
+    public int firstTest = 30;
+    public int secondTest = 35;
+    public int thirdTest = 35;
+
+
+}
+
+/*
+* Fazer um programa para ler o nome de um aluno e as três notas que ele obteve nos três trimestres do ano
+(primeiro trimestre vale 30 e o segundo e terceiro valem 35 cada). Ao final, mostrar qual a nota final do aluno no
+ano. Dizer também se o aluno está aprovado (PASS) ou não (FAILED) e, em caso negativo, quantos pontos faltam
+para o aluno obter o mínimo para ser aprovado (que é 60% da nota). Você deve criar uma classe Student para
+resolver este problema.
+*
+* 3 provas,  1 de 30 pontos, e duas de 35 pontos ttoal 105 pontos e se fixer menos de 60% desses pontos ele nãoé aprovado
+* caso ão tenha passado, exibir a pontuação alcançada e quantos pontos faltaram para os 60% */
