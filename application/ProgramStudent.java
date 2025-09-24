f4313c7ec651e1c8634e4bbb698509e1eca5f3b0
Pedro Vargas <96157565+Pedro-vandrade@users.noreply.github.com>
2025-09-24 14:14:54 -0300

    Update ProgramStudent.java

--- application/ProgramStudent.java
+++ application/ProgramStudent.java
@@ -1,4 +1,27 @@
 package application;
+import java.util.Scanner;
+import java.util.Locale;
+import entities.Student;
 
 public class ProgramStudent {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Student student = new Student();
+        System.out.println("Enter Student Name");
+        student.name = sc.nextLine();
+        student.firstTest = sc.nextDouble();
+        student.secondTest = sc.nextDouble();
+        student.thirdTest = sc.nextDouble();
+
+        System.out.printf("Final Grade: %.2f%n", student.finalGrade());
+        if(student.finalGrade()<60.){
+            System.out.println("Failed.");
+            System.out.printf("Missing points : %.2f%n", student.missingPoints());
+        }else {
+            System.out.println("Passed.");
+        }
+
+    }
 }
